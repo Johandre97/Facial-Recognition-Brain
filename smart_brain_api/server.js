0d5102f5d978a4d94f6f89253b5710fc4cb95f69
Johandre97 <johandrehdb@gmail.com>
2023-08-01 15:33:46 +0200

    latest upload, sign in confirmed, password hashing implemented

--- smart_brain_api/server.js
+++ smart_brain_api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bcrypt = require('bcrypt-nodejs')
 const app = express();
 
 
@@ -15,7 +16,6 @@ const database = {
             id: '123',
             name: 'john',
             email: 'john@gmail.com',
-            password: 'cookies',
             entries: 0,
             joined: new Date()
         },
@@ -23,9 +23,15 @@ const database = {
             id: '124',
             name: 'sally',
             email: 'sally@gmail.com',
-            password: 'bannanas',
             entries: 0,
             joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
         }
     ]
 }
@@ -36,6 +42,13 @@ app.get('/', (req, res) => {
 
 //sign in post module, compares with database email and password for a validation check.
 app.post('/signin', (req, res) => {
+    bcrypt.compare("apples", "$2a$10$MnCz.itBgJZvyTBTJBQxOu1vhb.BiTIkWaQyZufVhvxq3MpG5UEAq", function(err, res) {
+        // res == true
+        console.log('first guess', res)
+    });
+    bcrypt.compare("veggies", "$2a$10$MnCz.itBgJZvyTBTJBQxOu1vhb.BiTIkWaQyZufVhvxq3MpG5UEAq", function(err, res) {
+        console.log('second guess', res)
+    });
     if (req.body.email === database.users[0].email && 
         req.body.password === database.users[0].password) {
             res.json('success');
@@ -45,7 +58,10 @@ app.post('/signin', (req, res) => {
 })
 
 app.post('/register', (req,res) => {
-    const { email, name, password } = req.body; 
+    const { email, name, password } = req.body;
+    // bcrypt.hash(password, null, null, function(err, hash) {
+    //     console.log(hash);
+    // }); 
     database.users.push({
         id: '125',
         name: name,
