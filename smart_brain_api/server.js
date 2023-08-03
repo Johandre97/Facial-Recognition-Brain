391e18d63930ff9445510b1e8decd51d84179f90
Johandre97 <johandrehdb@gmail.com>
2023-08-03 23:27:30 +0200

    Connected PostgreSQL to app, working

--- smart_brain_api/server.js
+++ smart_brain_api/server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const bcrypt = require('bcrypt-nodejs')
 const cors = require('cors')
+const knex = require('knex')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'postgres',
+        port: 5432,
+        password: 'SQLPassX&7',
+        database: 'smart_brain'
+    }
+});
+
+db.select('*').from('users').then(data => {});
+
 const app = express();
 
 //need the json parser module that is contained here for express to interpret JSON.
@@ -10,102 +25,87 @@ app.use(express.json());
 
 app.use(cors())
 //this is the database that we will be comparing with.
-const database = {
-    users: [
-        {
-            id: '123',
-            name: 'John',
-            password: 'cookies',
-            email: 'john@gmail.com',
-            entries: 0,
-            joined: new Date()
-        },
-        {
-            id: '124',
-            name: 'Sally',
-            password: 'bannanas',
-            email: 'sally@gmail.com',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    // login: [
-    //     {
-    //         id: '987',
-    //         hash: '',
-    //         email: 'john@gmail.com'
-    //     }
-    // ]
-}
 
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('success')
 })
 
 //sign in post module, compares with database email and password for a validation check.
 app.post('/signin', (req, res) => {
-    // bcrypt.compare("apples", "$2a$10$MnCz.itBgJZvyTBTJBQxOu1vhb.BiTIkWaQyZufVhvxq3MpG5UEAq", function(err, res) {
-    //     // res == true
-    //     console.log('first guess', res)
-    // });
-    // bcrypt.compare("veggies", "$2a$10$MnCz.itBgJZvyTBTJBQxOu1vhb.BiTIkWaQyZufVhvxq3MpG5UEAq", function(err, res) {
-    //     console.log('second guess', res)
-    // });
-    if (req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password) {
-            res.json(database.users[0]); 
+    db.select('email', 'hash').from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if (isValid) {
+            return db.select('*').from('users')
+            .where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get user'))
         } else {
-            res.status(400).json('error logging in');
+        res.status(400).json('wrong credentials')
         }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+
 })
+
 
 app.post('/register', (req,res) => {
     const { email, name, password } = req.body;
-    // bcrypt.hash(password, null, null, function(err, hash) {
-    //     console.log(hash);
-    // }); 
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
-    })
-    res.json(database.users[database.users.length-1]);
+    const hash = bcrypt.hashSync(password)
+        db.transaction(trx => { //trx is a transaction, this ensures that the information is fed to 
+                                // both the login database, as well as the users.
+            trx.insert({
+                hash: hash,
+                email: email
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                .returning('*')
+                .insert({
+                    email : loginEmail[0].email, // this syntax is used with knex to ensure that the login email is not returned as an object to users.
+                    name: name,
+                    joined: new Date()
+                }).then(user => {
+                    res.json(user[0]);
+                })
+            })
+            .then(trx.commit) //in order for the transaction to be completed,it needs to be committed to the database
+            .catch(trx.rollback) // should it fail at one of the multiple actions in the transaction, it needs to rollback all changes.
+        })
+    .catch(err => res.status(400).json('unable to register'))
 })
 
 
 //the syntax below ':id' allows for the get request to use params 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    //there are better ways to do this, but this will allow the loop to exit when found
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
+    db.select('*').from('users').where({id})
+    .then(user => {
+        if (user.length){
+            res.json(user[0]);
+        } else {
+            res.status(400).json('Not found')
         }
     })
-    if (!found) {
-        res.status(400).json('user not found')
-    }
+    .catch(err=> res.status(400).json('error getting user'))
 })
 
 //this will keep count of the user image submissions
 app.put('/image', (req, res) => {
     const { id } = req.body;
     //this should actually be a function by now...
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++
-            return res.json(user.entries);
-        }
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0].entries);
     })
-    if (!found) {
-        res.status(400).json('user not found')
-    }
+    .catch(err => res.status(400).json('unable to get entries'))
 })
 
 //npm listener for changes/debugging.
